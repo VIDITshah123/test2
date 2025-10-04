c289d1983a740aab6c73f7be6e0e2af29e2df9e7
VIDITshah123 <shahvidit15@gmail.com>
2025-10-04 16:31:12 +0530

    chore: remove Firefox test configuration from Playwright setup

--- playwright.config.js
+++ playwright.config.js
@@ -31,9 +31,5 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
   ],
 });
